21357963a0efd057d1804d61736867bc51c99ed5
puppycat710 <puppycat710@proton.me>
2025-05-02 17:09:35 -0300

    gulp cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,32 @@
-const { src, dest, series } = require('gulp');
-const uglify = require('gulp-uglify');
-const minifyHtml = require('gulp-minify-html');
+const { src, dest, series } = require('gulp')
+const uglify = require('gulp-uglify')
+const minifyHtml = require('gulp-minify-html')
+var cssnano = require('gulp-cssnano')
 
 // Minify HTML
 const buildHTML = () => {
-  return src("./*.html")  // Caminho ajustado
-    .pipe(minifyHtml())
-    .pipe(dest("./")); // Sobrescreve os arquivos originais
+	return src('./*.html') // Caminho ajustado
+		.pipe(minifyHtml())
+		.pipe(dest('./')) // Sobrescreve os arquivos originais
 }
 
-// Minify HTML
-const buildFilms = () => {
-  return src("./filmes/**/*.html")  // Caminho ajustado
-    .pipe(minifyHtml())
-    .pipe(dest("./filmes")); // Sobrescreve os arquivos originais
-}
-
-// Minify HTML
-const buildSeries = () => {
-  return src("./series/**/*.html")  // Caminho ajustado
-    .pipe(minifyHtml())
-    .pipe(dest("./series")); // Sobrescreve os arquivos originais
-}
-/** 
 // CSSNano
 const buildCSS = () => {
-  return src("./css/*.css")
-    .pipe(cssnano())
-    .pipe(dest("./css/"));
+	return src('./css/*.css').pipe(cssnano()).pipe(dest('./css/'))
 }
 
 // Babel
-const buildBabel = () => {
-  return src('./js/*.js')
-    .pipe(babel())
-    .pipe(dest('./js/'));
-};
-**/
-// Uglify
-const buildJS = () => {
-  return src('./js/*.js')
-    .pipe(uglify())
-    .pipe(dest('./js/'));
-};
+// const buildBabel = () => {
+//   return src('./js/*.js')
+//     .pipe(babel())
+//     .pipe(dest('./js/'));
+// };
 
-exports.buildDev = series(buildHTML, buildJS, buildFilms, buildSeries);
+// Uglify
+// const buildJS = () => {
+//   return src('./js/*.js')
+//     .pipe(uglify())
+//     .pipe(dest('./js/'));
+// };
+
+exports.buildDev = series(buildHTML, buildCSS)
